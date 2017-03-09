f2a325a3f56ddbe15f04114de83925e49d7e44c9
pavoleg2009 <pavoleg2009@gmail.com>
2017-03-09 15:54:42 +0300

    empty database created

--- CustomerList/Controllers/CustomersController.cs
+++ CustomerList/Controllers/CustomersController.cs
@@ -3,15 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CustomerList.Models;
 
 namespace CustomerList.Controllers
 {
     public class CustomersController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ActionResult Index()
+        {   
+            var customers = new List<Customer>
+            {
+                new Customer { FristName = "John", LastName = "Smith"},
+                new Customer { FristName = "Barak", LastName = "Obama"},
+                new Customer { FristName = "Lucy", LastName = "Lyu"},
+                new Customer { FristName = "Poll", LastName = "Pott"},
+                new Customer { FristName = "Michel", LastName = "Jackson"}
+            };
+            return View(customers);
+        }
+
+        public ActionResult New(string s)
         {
-            return View();
+            throw new NotImplementedException();
         }
     }
 }
--- CustomerList/Models/Customer.cs
+++ CustomerList/Models/Customer.cs
@@ -14,13 +14,17 @@ namespace CustomerList.Models
         public int Id { get; set; }
 
         [Required]
+        [Display(Name = "First Name")]
         public string FristName { get; set; }
 
         [Required]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
+        [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
 
+        [EmailAddress]
         public string Email { get; set; }
 
     }
