1bae3af3c1c09813c41708bad9753d6587d84333
pavoleg2009 <pavoleg2009@gmail.com>
2017-03-09 14:27:40 +0300

    initial commit

---
+++ CustomerList/Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CustomerList.Models
+{
+    public class Customer
+    {
+
+
+
+        public int Id { get; set; }
+
+        [Required]
+        public string FristName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
