dc8e391e5d6f7447d60e444742a37fe32e217983
pavoleg2009 <pavoleg2009@gmail.com>
2017-03-09 14:20:00 +0300

    Add project files.

---
+++ CustomerList/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CustomerList.Startup))]
+namespace CustomerList
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
