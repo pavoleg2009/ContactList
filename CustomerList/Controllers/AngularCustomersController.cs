f53f3343514715f84eeac38704c74ba494965b3b
pavoleg2009 <pavoleg2009@gmail.com>
2017-03-12 12:03:41 +0300

    Angular begin

---
+++ CustomerList/Controllers/AngularCustomersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomerList.Controllers
+{
+    public class AngularCustomersController : Controller
+    {
+        // GET: AngularCustomers
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
